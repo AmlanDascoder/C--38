23e4c5bb4b8d7af4ea218fa80a3f30be858b642e
AmlanDascoder <78908469+AmlanDascoder@users.noreply.github.com>
2021-08-20 12:39:47 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,36 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name, 
+      distance:this.distance
+    });
+  }
+  //fetch all the data from the 4 players and store it in the local variable
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
